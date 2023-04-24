7a76380feb295892c15e9bcdf80265c3ce40f14d
Nischal Newar <nischalnewar009@gmail.com>
2023-04-24 14:59:46 +0000

    Fixed all the bugs

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -2,9 +2,17 @@ import classNames from "classnames"
 import { useRef } from "react"
 import { InputCheckboxComponent } from "./types"
 
-export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, disabled, onChange }) => {
+export const InputCheckbox: InputCheckboxComponent = ({
+  id,
+  checked = false,
+  disabled = false,
+  onChange,
+}) => {
   const { current: inputId } = useRef(`RampInputCheckbox-${id}`)
 
+  /* 
+    Task 2: The input tag should be inside the label to enable the checkbox feature.
+  */
   return (
     <div className="RampInputCheckbox--container" data-testid={inputId}>
       <label
@@ -12,15 +20,16 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
           "RampInputCheckbox--label-checked": checked,
           "RampInputCheckbox--label-disabled": disabled,
         })}
-      />
-      <input
-        id={inputId}
-        type="checkbox"
-        className="RampInputCheckbox--input"
-        checked={checked}
-        disabled={disabled}
-        onChange={() => onChange(!checked)}
-      />
+      >
+        <input
+          id={inputId}
+          type="checkbox"
+          className="RampInputCheckbox--input"
+          checked={checked}
+          disabled={disabled}
+          onChange={() => onChange(!checked)}
+        />
+      </label>
     </div>
   )
 }
